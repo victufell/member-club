3bc315ecdfe77baf35552c70fad11921b0675262
victufell <victor.fellype.a@gmail.com>
2024-10-14 18:59:43 -0400

    feat: adding function to get input value

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,14 @@
 import "../styles/index.css"
+
+const inputIdentify = document.querySelector<HTMLInputElement>('[data-identify="user-id"]')
+
+const handleInputChange = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    const inputValue = target?.value
+
+    console.log({inputValue})
+}
+
+inputIdentify?.addEventListener('change', handleInputChange)
+
+console.log("inputIdentify", inputIdentify)
